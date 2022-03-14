03c4a4bfd6904e72fe62569c55775505dd7a9eb3
xxrjun <rjun0729@gmail.com>
2022-03-14 22:59:55 +0800

    react chat app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
+import ChatFeed from "./components/ChatFeed";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height="100vh"
+      publicKey={process.env.REACT_APP_PROJECT_ID}
+      userName="test001"
+      userSecret="test001"
+      renderChatFeed={(chatFeedProps) => <ChatFeed {...chatFeedProps} />}
+    />
   );
 }
 
---
+++ src/components/MyMessage.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function MyMessage({ message }) {
+  // If message is a image, then return a img
+  if (message?.attachments?.length > 0) {
+    return (
+      <img
+        src={message.attachments[0].file}
+        alt="message-attachment"
+        className="message-image"
+        style={{ float: "right" }}
+      />
+    );
+  }
+  // Else return a text message
+  return (
+    <div
+      className="message"
+      style={{
+        float: "right",
+        marginRight: "18px",
+        color: "white",
+        backgroundColor: "#3B2A50",
+      }}
+    >
+      {message.text}
+    </div>
+  );
+}
+
+export default MyMessage;
